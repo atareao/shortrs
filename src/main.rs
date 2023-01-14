8c4b22679e19afbc4453024fe7ed99ea8586ccea
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-01-14 09:35:22 +0100

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use config::Configuration;
+use tracing_subscriber::EnvFilter;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use sqlx::{sqlite::SqlitePoolOptions, migrate::{Migrator, MigrateDatabase}};
+use std::{env, path::Path};
+use std::str::FromStr;
+use tracing::info;
+
+mod model;
+mod config;
+mod http;
+
+#[tokio::main]
+async fn main(){
+    let configuration = Configuration::read().await;
+
+    tracing_subscriber::registry()
+        .with(EnvFilter::from_str(configuration.get_log_level()).unwrap())
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    let db_url = configuration.get_db_url();
+
+    if !sqlx::Sqlite::database_exists(db_url).await.unwrap(){
+        sqlx::Sqlite::create_database(db_url).await.unwrap();
+    }
+    let migrations = if env::var("RUST_ENV") == Ok("production".to_string()){
+        std::env::current_exe().unwrap().parent().unwrap().join("migrations")
+    }else{
+        let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+        Path::new(&crate_dir).join("./migrations")
+    };
+    info!("{}", &migrations.display());
+    let pool = SqlitePoolOptions::new()
+        .max_connections(2)
+        .connect(db_url)
+        .await
+        .expect("Pool failed");
+
+    Migrator::new(migrations)
+        .await
+        .unwrap()
+        .run(&pool)
+        .await
+        .unwrap();
+    http::serve(configuration, pool).await.unwrap();
 }
