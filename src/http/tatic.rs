e51354f45cd80b29ccba5910241dc65c6c9b1389
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-01-22 22:50:01 +0100

    url http

---
+++ src/http/tatic.rs
@@ -0,0 +1,19 @@
+use axum::{
+    Router,
+    routing::get_service,
+    http::StatusCode,
+    response::IntoResponse,
+};
+use tower_http::services::ServeDir;
+
+pub fn router() -> Router {
+    Router::new()
+        .nest_service(
+            "/assets",
+            get_service(ServeDir::new("./assets"))
+        .handle_error(handle_error))
+}
+async fn handle_error(_err: tokio::io::Error) -> impl IntoResponse {
+    (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong...")
+}
+
