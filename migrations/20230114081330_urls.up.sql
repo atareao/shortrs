52f7ed9629ed04dec5b22ec9dc77301bdcc1884c
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-01-22 22:49:40 +0100

    update migration

--- migrations/20230114081330_urls.up.sql
+++ migrations/20230114081330_urls.up.sql
@@ -1,9 +1,8 @@
 -- Add up migration script here
 CREATE TABLE IF NOT EXISTS urls(
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    channel_id TEXT NOT NULL,
+    url_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     src TEXT NOT NULL UNIQUE,
-    num TEXT NOT NULL,
+    num INTEGER NOT NULL,
     active BOOLEAN NOT NULL,
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL
